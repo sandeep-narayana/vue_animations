f05542988059307a8cf3ee58ff789925a7b80d6a
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-19 16:53:25 +0530

    formate

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="todos">
-    <input 
-      type="text" 
-      v-model="newTodo" 
+    <input
+      type="text"
+      v-model="newTodo"
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
@@ -18,65 +18,65 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
   setup(props, { emit }) {
     const todos = ref([
-      { text: 'make the bed', id: 1 },
-      { text: 'play video games', id: 2 },
-    ])
-    const newTodo = ref('')
+      { text: "make the bed", id: 1 },
+      { text: "play video games", id: 2 },
+    ]);
+    const newTodo = ref("");
 
     const addTodo = () => {
       if (newTodo.value) {
-        const id = Math.random()
-        todos.value = [{ text: newTodo.value, id }, ...todos.value]
-        newTodo.value = ''
+        const id = Math.random();
+        todos.value = [{ text: newTodo.value, id }, ...todos.value];
+        newTodo.value = "";
       } else {
-        emit('badValue')
+        emit("badValue");
       }
-    }
+    };
 
     const deleteTodo = (id) => {
-      todos.value = todos.value.filter(todo => todo.id != id)
-    }
+      todos.value = todos.value.filter((todo) => todo.id != id);
+    };
 
-    return { todos, addTodo, deleteTodo, newTodo }
-  }
-}
+    return { todos, addTodo, deleteTodo, newTodo };
+  },
+};
 </script>
 
 <style>
-  .todos {
-    max-width: 400px;
-    margin: 20px auto;
-    position: relative;
-  }
-  input {
-    width: 100%;
-    padding: 12px;
-    border: 1px solid #eee;
-    border-radius: 10px;
-    box-sizing: border-box;
-    margin-bottom: 20px;
-  }
-  .todos ul {
-    position: relative;
-    padding: 0;
-  }
-  .todos li {
-    list-style-type: none;
-    display: block;
-    margin-bottom: 10px;
-    padding: 10px;
-    background: white;
-    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-    border-radius: 10px;
-    width: 100%;
-    box-sizing: border-box;
-  }
-  .todos li:hover {
-    cursor: pointer;
-  }
+.todos {
+  max-width: 400px;
+  margin: 20px auto;
+  position: relative;
+}
+input {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #eee;
+  border-radius: 10px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+}
+.todos ul {
+  position: relative;
+  padding: 0;
+}
+.todos li {
+  list-style-type: none;
+  display: block;
+  margin-bottom: 10px;
+  padding: 10px;
+  background: white;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+.todos li:hover {
+  cursor: pointer;
+}
 </style>
