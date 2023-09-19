d881b9e2a2b1408c450ffdd696eb739ecac83b79
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-19 19:22:59 +0530

    how to use GSAP library

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Contact from '../views/Contact.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contact from "../views/Contact.vue";
+import About2 from "../views/About2.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact
-  }
-]
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
+  {
+    path: "/about2",
+    name: "About2",
+    component: About2,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/About2.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="about">
+    <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+    >
+      <!-- to check the leave hooks -->
+      <h1>About - 2</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+export default {
+  setup() {
+    // bounce effect
+    const beforeEnter = (el) => {
+      console.log("before enter - set initial state");
+      el.style.transform = "translateY(-60px)";
+      el.style.opacity = 0;
+    };
+
+    // second argumnet to tell view that the enter animation is finished otherwise it will trigger the after Enter
+    const enter = (el, done) => {
+      //   use g sap otherwise animation will not work
+      console.log("starting to enter - make transition");
+      gsap.to(el, {
+        duration: 3,
+        y: 0,
+        opacity: 1,
+        ease: "bounce.out",
+        onComplete: done,
+      });
+    };
+
+    //
+    const afterEnter = () => {
+      console.log("after enter");
+    };
+
+    return {
+      beforeEnter,
+      enter,
+      afterEnter,
+    };
+  },
+};
+</script>
+
+<style>
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+.fade-leave-from {
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity 3s ease;
+}
+</style>
