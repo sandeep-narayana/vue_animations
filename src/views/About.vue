97733f5ce03a4e2ef0ec22af3728eb55baedfbd3
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-19 18:14:57 +0530

    about title fades in while refresh or change the route

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition name="fade" appear>
+      <h1>About</h1>
+    </transition>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
       officia possimus delectus inventore quod quisquam culpa voluptas iusto,
@@ -32,5 +34,22 @@ export default {
 .about {
   max-width: 600px;
   margin: 20px auto;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+.fade-leave-from {
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity 3s ease;
 }
 </style>
